df6021db74ef34f173b83b4563ccc20602963b3c
44758sanderl <44758@edu.sde.dk>
2026-09-07 09:12:13 +0200

    Works can print

--- FiveWordFiveLetters/Program.cs
+++ FiveWordFiveLetters/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace FiveWordFiveLetters
+﻿namespace FiveWordFiveLetters;
+
+using System.IO;
+
+internal class Program
+
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        StreamReader sr = new StreamReader("PerfectList.txt");
+        while (sr.EndOfStream == false)
         {
-            
+            String line = sr.ReadLine();
+            Console.WriteLine(line);
         }
+        
+
     }
 }
